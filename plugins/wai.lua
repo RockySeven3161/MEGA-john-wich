b4c3b0f7d28445818d2c63e0e38318c90454352b
RockySeven3161 <babaei.alireza580@GMAIL.COM>
2016-04-12 21:35:58 +0430

    Update wai.lua

--- plugins/wai.lua
+++ plugins/wai.lua
@@ -1,5 +1,5 @@
 do function run(msg, matches)
-
+   
    if matches[1]:lower() == 'wai' and is_sudo(msg) then
       return "You are SUDO"
       end
@@ -15,36 +15,16 @@ do function run(msg, matches)
       if matches[1]:lower() == 'wai' then
       return "You are Group Member"
     end
-      if matches[1]:lower() == '����' and is_sudo(msg) then
-      return "��� ������ ���� �����"
-      end
-      if matches[1]:lower() == '����' and is_admin(msg) then
-      return "��� ����� ���� �����"
-      end
-      if matches[1]:lower() == '����' and is_owner(msg) then
-      return "��� ���� ���� ���� �����"
-      end
-      if matches[1]:lower() == '����' and is_momod(msg) then
-      return "��� �� ���� �����"
-      end
-      if matches[1]:lower() == '����' then
-      return "��� ��� ���� �����"
-    end
 end
   return {
   description = "Your Postation In Group",
   usage = "Wai",
   patterns = {
   "^([Ww][Aa][Ii])$",
-  "^(����)$"
  , "^([Ww][Aa][Ii])$",
-  "^(����)$"
  , "^([Ww][Aa][Ii])$",
-  "^(����)$"
  , "^([Ww][Aa][Ii])$",
-  "^(����)$"
  , "^([Ww][Aa][Ii])$",
-  "^(����)$"
     },
   run = run
 }
